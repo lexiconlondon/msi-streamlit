32ce4d1c4c3fd8164709ca6916c586bc572fcf0e
Lexicon London <james@lexiconlondon.com>
2024-10-30 15:51:06 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -39,9 +39,9 @@ fig, ax = plt.subplots(figsize=(14, 10))
 for i, (country, y_pos) in enumerate(zip(likelihood_df.index, y_positions)):
     for j, category in enumerate(likelihood_df.columns):
         ax.scatter(j, y_pos,
-                   s=likelihood_df.loc[country, category] * 10,  # Scale for bubble size
+                   s=likelihood_df.loc[country, category] * 20,  # Scale for bubble size
                    color=color_discrete_sequence[i % len(color_discrete_sequence)],  # Reversed color order
-                   alpha=0.6,
+                   alpha=1,
                    edgecolor='black',
                    marker='o')  # Circular marker
 
